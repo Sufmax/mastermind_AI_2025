bd63d71d946088d240d3756ccb288828732c6435
Sufmax <126913399+Sufmax@users.noreply.github.com>
2025-10-20 19:02:32 +0200

    Simplify train function and update checkpoint handling
    Refactor training function to remove TensorBoard integration and update checkpointing method.

--- train.py
+++ train.py
@@ -1,93 +1,48 @@
-import tensorflow.contrib.eager as tfe
 import tensorflow as tf
 import random
-import argparse
 import os
-import ipdb
 import sys
-
-from episode import Episode
 from policy import Policy
+from episode import Episode
 import config
 
-
-
-def train(num_episodes=1000, 
-          save_every=100, 
-          checkpoint_dir="checkpoints",
-          tensorboard_dir="tensorboard",
-          tboard_every=10):
+def train(num_episodes=1000, save_every=100, checkpoint_dir="checkpoints"):
     pol = Policy()
-    writer = tf.contrib.summary.create_file_writer(tensorboard_dir)
-    for j in range(1, num_episodes+1):
+    optimizer = tf.keras.optimizers.SGD(learning_rate=config.reinforce_alpha)
+
+    # checkpoint manager
+    ckpt = tf.train.Checkpoint(model=pol, optimizer=optimizer)
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_dir, max_to_keep=5)
+
+    for j in range(1, num_episodes + 1):
         random_secret = random.randint(0, config.max_guesses - 1)
         e = Episode(pol, random_secret)
         history = e.generate()
 
-        print("Episode length: {}".format(len(history)))
+        print("Episode length:", len(history))
 
-        G = -1 
-
-        optimizer = \
-            tf.train.GradientDescentOptimizer(
-                learning_rate=config.reinforce_alpha*G)
+        G = -1  # return
 
         for i in reversed(range(1, len(history))):
             history_so_far = history[:i]
             next_action, _ = history[i]
-            with tfe.GradientTape() as tape:
-                action_logits = pol(history_so_far, with_softmax=False)
-                loss = tf.nn.softmax_cross_entropy_with_logits_v2(
-                    labels=tf.one_hot(
-                        tf.convert_to_tensor([next_action]),
-                        config.max_guesses),
-                    logits=action_logits
-                )
+
+            with tf.GradientTape() as tape:
+                logits = pol(history_so_far, with_softmax=False)
+                labels = tf.one_hot([next_action], config.max_guesses)
+                loss = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits)
 
             grads = tape.gradient(loss, pol.variables)
             optimizer.apply_gradients(zip(grads, pol.variables))
-
             G -= 1
-            optimizer._learning_rate = G * config.reinforce_alpha
-            optimizer._learning_rate_tensor = None
-            # hack. Should be able to pass a callable as learning_rate, see
-            # https://www.tensorflow.org/api_docs/python/tf/train/GradientDescentOptimizer#args
-            # can I perhaps submit a PR to fix this bug?
 
             sys.stdout.write("{}/{}\r".format(len(history)-i, len(history)))
+            sys.stdout.flush()
 
+        # save checkpoint
         if j % save_every == 0 or j == num_episodes:
-            saver = tfe.Saver(pol.named_variables)
-            save_path = os.path.join(checkpoint_dir, 
-                                     "episode{}".format(
-                                         str(j).zfill(len(str(num_episodes)))))
-            saver.save(save_path)
-
-        if j % tboard_every == 0:
-            with writer.as_default():
-                with tf.contrib.summary.always_record_summaries():
-                    tf.contrib.summary.scalar('total_return', 
-                                              tf.convert_to_tensor([G]), 
-                                              step=j)
-
+            ckpt_manager.save()
+            print(f"\nCheckpoint saved at episode {j}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Train the mastermind model "
-                                     "Using the REINFORCE policy gradient "
-                                     "method")
-    parser.add_argument("--num_episodes", 
-                        help="Number of episodes to use for training",
-                        type=int, default=1000)
-    parser.add_argument("--save_every", type=int, default=100,
-                        help="Checkpoint every N episodes")
-    parser.add_argument("--checkpoint_dir", help="Checkpoint directory",
-                        default="checkpoints")
-    parser.add_argument("--board_dir", help="Tensorboard directory",
-                        default="tensorboard")
-    parser.add_argument("--tboard_every", type=int, default=10,
-                        help="Write to tensorboard every N episodes")
-    args = parser.parse_args()
-                                  
-    train(args.num_episodes, args.save_every, args.checkpoint_dir,
-          args.board_dir, args.tboard_every)
-
+    train()
